7f5f64978db3b795ca497e7dd26c569c917602c0
David Oberhollenzer <david.oberhollenzer@tele2.at>
2018-04-19 07:23:15 +0200

    cleanup: restructure user directories
    Instead of having an overlay mounted /root in the root filesystem,
    move it to the other user directories, i.e. /usr/root.
    
    We then overlay mount the entire /usr directory. This still allows
    us to mount /usr from somewhere else if we wish, but cleans
    up/simplifies the filesystem for now and implements some default
    management of general user directories.
    
    Signed-off-by: David Oberhollenzer <david.oberhollenzer@tele2.at>

--- pkg/release-alix/genimage.sh
+++ pkg/release-alix/genimage.sh
@@ -51,9 +51,9 @@ mount -t btrfs ${LODEV}p2 /tmp/mnt.$$
 MOUNTED=1
 mkdir /tmp/mnt.$$/etc /tmp/mnt.$$/etc_work
 mkdir /tmp/mnt.$$/var_lib /tmp/mnt.$$/var_lib_work
-mkdir /tmp/mnt.$$/root /tmp/mnt.$$/root_work
-chmod 750 /tmp/mnt.$$/root /tmp/mnt.$$/root_work
-chown 0:0 /tmp/mnt.$$/root /tmp/mnt.$$/root_work
+mkdir -p /tmp/mnt.$$/usr/root /tmp/mnt.$$/usr_work
+chmod 750 /tmp/mnt.$$/usr/root /tmp/mnt.$$/usr_work
+chown 0:0 /tmp/mnt.$$/usr/root
 umount /tmp/mnt.$$
 MOUNTED=0
 
