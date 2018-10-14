7b0e1f182cd96903563f11c344543908a35a9b7a
David Oberhollenzer <david.oberhollenzer@tele2.at>
2018-10-14 16:55:36 +0200

    Add hardening flags to autotools based packages
    Signed-off-by: David Oberhollenzer <david.oberhollenzer@tele2.at>

--- util/autotools.sh
+++ util/autotools.sh
@@ -2,8 +2,12 @@ run_configure() {
 	local srcdir="$1"
 	shift
 
+	local cflags="-fstack-protector-all"
+	local ldflags="-z noexecstack -z relro -z now"
+
 	ac_cv_func_malloc_0_nonnull=yes \
 	ac_cv_func_realloc_0_nonnull=yes \
+	CFLAGS="$cflags" LDFLAGS="$ldflags" \
 	$srcdir/configure --prefix="" --build="$HOSTTUPLE" --host="$TARGET" \
 		--bindir="/bin" --sbindir="/bin" --sysconfdir="/etc" \
 		--libexecdir="/lib/libexec" --datarootdir="/share" \
