a9eb8929201f429a3df873e29773fe300f19fc68
David Oberhollenzer <goliath@infraroot.at>
2019-03-06 21:15:42 +0100

    cleanup: use package files instead of source package dotfiles
    Try to build a source package if at least one binary package is missing. If
    that is the case, clean up possibly built packages so we can cleanly recover
    from a prior crash.
    
    Signed-off-by: David Oberhollenzer <goliath@infraroot.at>

--- mk.sh
+++ mk.sh
@@ -89,31 +89,41 @@ done < "$PACKAGELIST"
 echo "--- building packages ---"
 
 while read pkg; do
-	if [ ! -e "$PKGLOGDIR/.$pkg" ]; then
-		include_pkg "$pkg"
+	include_pkg "$pkg"
 
-		rm -rf "$TCDIR/$TARGET" "$PKGBUILDDIR" "$PKGDEPLOYDIR"
-		mkdir -p "$TCDIR/$TARGET" "$PKGBUILDDIR" "$PKGDEPLOYDIR"
+	found="yes"
 
-		if [ ! -z "$DEPENDS" ]; then
-			pkg install -omD -r "$TCDIR/$TARGET" -R "$REPODIR" $DEPENDS
+	for f in $SUBPKG; do
+		if [ ! -f "$REPODIR/${f}.pkg" ]; then
+			found="no"
+			break
 		fi
+	done
 
-		run_pkg_command "build"
-		run_pkg_command "deploy"
-		deploy_dev_cleanup
-		strip_files ${PKGDEPLOYDIR}/{bin,lib}
+	[ "x$found" == "xno" ] || continue;
 
-		for f in $SUBPKG; do
-			pkg pack -r "$REPODIR" \
-			    -d "$PKGDEPLOYDIR/${f}.desc" \
-			    -l "$PKGDEPLOYDIR/${f}.files"
-		done
+	for f in $SUBPKG; do
+		rm -f "$REPODIR/${f}.pkg"
+	done
 
-		rm -rf "$TCDIR/$TARGET" "$PKGBUILDDIR" "$PKGDEPLOYDIR"
-		touch "$PKGLOGDIR/.$pkg"
+	rm -rf "$TCDIR/$TARGET" "$PKGBUILDDIR" "$PKGDEPLOYDIR"
+	mkdir -p "$TCDIR/$TARGET" "$PKGBUILDDIR" "$PKGDEPLOYDIR"
+
+	if [ ! -z "$DEPENDS" ]; then
+		pkg install -omD -r "$TCDIR/$TARGET" -R "$REPODIR" $DEPENDS
 	fi
+
+	run_pkg_command "build"
+	run_pkg_command "deploy"
+	deploy_dev_cleanup
+	strip_files ${PKGDEPLOYDIR}/{bin,lib}
+
+	for f in $SUBPKG; do
+		pkg pack -r "$REPODIR" -d "$PKGDEPLOYDIR/${f}.desc" \
+			 -l "$PKGDEPLOYDIR/${f}.files"
+	done
+
+	rm -rf "$TCDIR/$TARGET" "$PKGBUILDDIR" "$PKGDEPLOYDIR"
 done < "$PACKAGELIST"
 
 echo "--- done ---"
-
